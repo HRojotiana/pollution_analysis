170103071ff4b7d6736bb90d9772360c39581f9c
HRojotiana <hei.rojotiana@gmail.com>
2024-08-09 16:12:45 +0300

    feat: remove_duplicates function

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from extraction.extract_pollution_data import extract_geographic_pollution, extract_demographic_pollution
+from transformation.transform import remove_duplicated_lines
+from loading.load_data import load_demographic_and_pollutions_in_database
 
-print(extract_geographic_pollution())
+df = extract_demographic_pollution()
+print(df.duplicated())
+print(f"Duplicated values number : {df.duplicated().sum()}")
+no_duplicates = remove_duplicated_lines(df)
+print(no_duplicates)
+print(f"Duplicated values number : {no_duplicates.duplicated().sum()}")
+
+# load_demographic_and_pollutions_in_database()
